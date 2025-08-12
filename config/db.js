46488222f656d1c82ec3672eff1e5a5064f6e2fb
Aaron Estrada <ryaaron000@gmail.com>
2025-08-12 13:28:25 -0600

    completamente listo no mover

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/seguimiento', {
+    await mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://Aaron:AARtre78@cluster0.ztns0jl.mongodb.net/seguimiento?retryWrites=true&w=majority', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       autoIndex: true // Habilitar la creación automática de índices
